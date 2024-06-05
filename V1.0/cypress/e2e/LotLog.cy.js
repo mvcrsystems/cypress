fa486714ee5c050edded7850c788e07d5cf55bb0
kevir1502 <33071671+kevir1502@users.noreply.github.com>
2024-06-04 21:17:12 -0600

    ff

--- V1.0/cypress/e2e/LotLog.cy.js
+++ V1.0/cypress/e2e/LotLog.cy.js
@@ -3,7 +3,6 @@ describe('Lot Log Interface', () => {
     cy.visit('http://localhost:3000/trackerPro/LotLog');
     cy.get('#building').select('CER2');
     cy.get("#btnBuilding").click();
-    cy.wait(timeWait(1000, 5000));
   });
 
   var consecutive = 0;
@@ -24,68 +23,96 @@ describe('Lot Log Interface', () => {
   }
 
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} Enter a wrong batch number`, function () {
+    cy.get('#batchNumber').type('00003991200');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+  });
+  sumConsecutive();
+  it(`${consecutive} The system display error message 'There is no associated recipe!'`, function () {
+    cy.get('#batchNumber').type('00003991200');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+  });
+  sumConsecutive();
+  it(`${consecutive} Enter correct batch number`, function () {
+    cy.get('#batchNumber').type('0000399120');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+  });
+  sumConsecutive();
+  it(`${consecutive} Enter negative amount`, function () {
+    cy.get('#batchNumber').type('0000399120');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+    cy.get('#qty').type('-1');
+    cy.get('#qty').type('{enter}');
+    timeWait(1000, 5000);
+  });
+  sumConsecutive();
+  it(`${consecutive} The system display error message ''`, function () {
+    cy.get('#batchNumber').type('0000399120');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+    cy.get('#qty').type('-1');
+    cy.get('#qty').type('{enter}');
+    timeWait(1000, 5000);
+  });
+  sumConsecutive();
+  it(`${consecutive} Enter correct amount`, function () {
+    cy.get('#batchNumber').type('0000399120');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+    cy.get('#qty').type('10');
+    cy.get('#qty').type('{enter}');
+    timeWait(1000, 5000);
+  });
+  sumConsecutive();
+  it(`${consecutive} Complete initial production line clearance`, function () {
+    cy.get('#batchNumber').type('0000399120');
+    cy.get('#batchNumber').type('{enter}');
+    timeWait(1000, 5000);
+    cy.get('#qty').type('10');
+    cy.get('#qty').type('{enter}');
+    timeWait(1000, 5000);
+    cy.get('#initialClearance').click();
+  });
+  sumConsecutive();
+  it(`${consecutive} Enter wrong user id`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} System display error message ''`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} Enter correct user id`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} Enter wrong pin`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} System display error message ''`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} Enter correct pin`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} Click on save button `, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} System display message`, function () {
 
   });
   sumConsecutive();
-  it(`${consecutive}`, function () {
+  it(`${consecutive} The lot number entered appears on the batch number history table`, function () {
 
   });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
 
-  });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
-
-  });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
-
-  });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
-
-  });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
-
-  });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
-
-  });
-  sumConsecutive();
-  it(`${consecutive}`, function () {
-
-  });
 
 });
